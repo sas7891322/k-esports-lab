6d80ab46f8f4532feecba1cdacc0e3a2eb552a9b
sas7891322 <sas7891322@gmail.com>
2026-08-12 11:03:42 +0800

    K Esports Lab v1.7 complete 6-league team logo database

--- assets/js/teams.js
+++ assets/js/teams.js
@@ -154,5 +154,141 @@ window.KEL_TEAMS = {
       "short": "DCG",
       "logo": "assets/img/teams/lcp/dcg.png"
     }
+  },
+  "LEC": {
+    "KC": {
+      "name": "Karmine Corp",
+      "short": "KC",
+      "logo": "assets/img/teams/lec/kc.png"
+    },
+    "G2": {
+      "name": "G2 Esports",
+      "short": "G2",
+      "logo": "assets/img/teams/lec/g2.png"
+    },
+    "VIT": {
+      "name": "Team Vitality",
+      "short": "VIT",
+      "logo": "assets/img/teams/lec/vit.png"
+    },
+    "GX": {
+      "name": "GIANTX",
+      "short": "GX",
+      "logo": "assets/img/teams/lec/gx.png"
+    },
+    "NAVI": {
+      "name": "Natus Vincere",
+      "short": "NAVI",
+      "logo": "assets/img/teams/lec/navi.png"
+    },
+    "MKOI": {
+      "name": "Movistar KOI",
+      "short": "MKOI",
+      "logo": "assets/img/teams/lec/mkoi.png"
+    },
+    "SK": {
+      "name": "SK Gaming",
+      "short": "SK",
+      "logo": "assets/img/teams/lec/sk.png"
+    },
+    "TH": {
+      "name": "Team Heretics",
+      "short": "TH",
+      "logo": "assets/img/teams/lec/th.png"
+    },
+    "SHFT": {
+      "name": "Shifters",
+      "short": "SHFT",
+      "logo": "assets/img/teams/lec/shft.png"
+    },
+    "FNC": {
+      "name": "Fnatic",
+      "short": "FNC",
+      "logo": "assets/img/teams/lec/fnc.png"
+    }
+  },
+  "LCS": {
+    "TLAW": {
+      "name": "Team Liquid Alienware",
+      "short": "TLAW",
+      "logo": "assets/img/teams/lcs/tlaw.png"
+    },
+    "LYON": {
+      "name": "LYON",
+      "short": "LYON",
+      "logo": "assets/img/teams/lcs/lyon.png"
+    },
+    "FLY": {
+      "name": "FlyQuest",
+      "short": "FLY",
+      "logo": "assets/img/teams/lcs/fly.png"
+    },
+    "C9": {
+      "name": "Cloud9 Kia",
+      "short": "C9",
+      "logo": "assets/img/teams/lcs/c9.png"
+    },
+    "SEN": {
+      "name": "Sentinels",
+      "short": "SEN",
+      "logo": "assets/img/teams/lcs/sen.png"
+    },
+    "SR": {
+      "name": "Shopify Rebellion",
+      "short": "SR",
+      "logo": "assets/img/teams/lcs/sr.png"
+    },
+    "DSG": {
+      "name": "Disguised",
+      "short": "DSG",
+      "logo": "assets/img/teams/lcs/dsg.png"
+    },
+    "DIG": {
+      "name": "Dignitas",
+      "short": "DIG",
+      "logo": "assets/img/teams/lcs/dig.png"
+    }
+  },
+  "CBLOL": {
+    "LOS": {
+      "name": "Los Grandes",
+      "short": "LOS",
+      "logo": "assets/img/teams/cblol/los.png"
+    },
+    "LOUD": {
+      "name": "LOUD",
+      "short": "LOUD",
+      "logo": "assets/img/teams/cblol/loud.png"
+    },
+    "VKS": {
+      "name": "Vivo Keyd Stars",
+      "short": "VKS",
+      "logo": "assets/img/teams/cblol/vks.png"
+    },
+    "FUR": {
+      "name": "FURIA",
+      "short": "FUR",
+      "logo": "assets/img/teams/cblol/fur.png"
+    },
+    "LEV": {
+      "name": "Leviatán",
+      "short": "LEV",
+      "logo": "assets/img/teams/cblol/lev.png"
+    },
+    "W7M": {
+      "name": "Fluxo W7M",
+      "short": "W7M",
+      "logo": "assets/img/teams/cblol/w7m.png"
+    },
+    "PNG": {
+      "name": "paiN Gaming",
+      "short": "PNG",
+      "logo": "assets/img/teams/cblol/png.png"
+    },
+    "RED": {
+      "name": "RED Canids Kalunga",
+      "short": "RED",
+      "logo": "assets/img/teams/cblol/red.png"
+    }
   }
 };
