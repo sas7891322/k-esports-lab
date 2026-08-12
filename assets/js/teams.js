14e4fa4c1bbb98c00e817c240f006252e1fef853
sas7891322 <sas7891322@gmail.com>
2026-08-12 09:03:55 +0800

    K Esports Lab v1.5 LPL LCK LCP team logo database

---
+++ assets/js/teams.js
@@ -0,0 +1,158 @@
+window.KEL_TEAMS = {
+  "LPL": {
+    "BLG": {
+      "name": "Bilibili Gaming",
+      "short": "BLG",
+      "logo": "assets/img/teams/lpl/blg.png"
+    },
+    "TES": {
+      "name": "Top Esports",
+      "short": "TES",
+      "logo": "assets/img/teams/lpl/tes.png"
+    },
+    "AL": {
+      "name": "Anyone's Legend",
+      "short": "AL",
+      "logo": "assets/img/teams/lpl/al.png"
+    },
+    "WE": {
+      "name": "Team WE",
+      "short": "WE",
+      "logo": "assets/img/teams/lpl/we.png"
+    },
+    "JDG": {
+      "name": "JD Gaming",
+      "short": "JDG",
+      "logo": "assets/img/teams/lpl/jdg.png"
+    },
+    "TT": {
+      "name": "ThunderTalk Gaming",
+      "short": "TT",
+      "logo": "assets/img/teams/lpl/tt.png"
+    },
+    "LGD": {
+      "name": "LGD Gaming",
+      "short": "LGD",
+      "logo": "assets/img/teams/lpl/lgd.png"
+    },
+    "EDG": {
+      "name": "Edward Gaming",
+      "short": "EDG",
+      "logo": "assets/img/teams/lpl/edg.png"
+    },
+    "NIP": {
+      "name": "Ninjas in Pyjamas",
+      "short": "NIP",
+      "logo": "assets/img/teams/lpl/nip.png"
+    },
+    "IG": {
+      "name": "Invictus Gaming",
+      "short": "IG",
+      "logo": "assets/img/teams/lpl/ig.png"
+    },
+    "WBG": {
+      "name": "Weibo Gaming",
+      "short": "WBG",
+      "logo": "assets/img/teams/lpl/wbg.png"
+    },
+    "LNG": {
+      "name": "LNG Esports",
+      "short": "LNG",
+      "logo": "assets/img/teams/lpl/lng.png"
+    }
+  },
+  "LCK": {
+    "DK": {
+      "name": "Dplus KIA",
+      "short": "DK",
+      "logo": "assets/img/teams/lck/dk.png"
+    },
+    "GEN": {
+      "name": "Gen.G Esports",
+      "short": "GEN",
+      "logo": "assets/img/teams/lck/gen.png"
+    },
+    "T1": {
+      "name": "T1",
+      "short": "T1",
+      "logo": "assets/img/teams/lck/t1.png"
+    },
+    "KT": {
+      "name": "KT Rolster",
+      "short": "KT",
+      "logo": "assets/img/teams/lck/kt.png"
+    },
+    "HLE": {
+      "name": "Hanwha Life Esports",
+      "short": "HLE",
+      "logo": "assets/img/teams/lck/hle.png"
+    },
+    "BFX": {
+      "name": "BNK FEARX",
+      "short": "BFX",
+      "logo": "assets/img/teams/lck/bfx.png"
+    },
+    "NS": {
+      "name": "Nongshim RedForce",
+      "short": "NS",
+      "logo": "assets/img/teams/lck/ns.png"
+    },
+    "BRO": {
+      "name": "Hanjin BRION",
+      "short": "BRO",
+      "logo": "assets/img/teams/lck/bro.png"
+    },
+    "DNS": {
+      "name": "DN SOOPers",
+      "short": "DNS",
+      "logo": "assets/img/teams/lck/dns.png"
+    },
+    "KRX": {
+      "name": "KIWOOM DRX",
+      "short": "KRX",
+      "logo": "assets/img/teams/lck/krx.png"
+    }
+  },
+  "LCP": {
+    "GZ": {
+      "name": "Ground Zero Gaming",
+      "short": "GZ",
+      "logo": "assets/img/teams/lcp/gz.png"
+    },
+    "MVK": {
+      "name": "MVK Esports",
+      "short": "MVK",
+      "logo": "assets/img/teams/lcp/mvk.png"
+    },
+    "SHG": {
+      "name": "Fukuoka SoftBank HAWKS gaming",
+      "short": "SHG",
+      "logo": "assets/img/teams/lcp/shg.png"
+    },
+    "TSW": {
+      "name": "Team Secret Whales",
+      "short": "TSW",
+      "logo": "assets/img/teams/lcp/tsw.png"
+    },
+    "DFM": {
+      "name": "DetonatioN FocusMe",
+      "short": "DFM",
+      "logo": "assets/img/teams/lcp/dfm.png"
+    },
+    "GAM": {
+      "name": "GAM Esports",
+      "short": "GAM",
+      "logo": "assets/img/teams/lcp/gam.png"
+    },
+    "CFO": {
+      "name": "CTBC Flying Oyster",
+      "short": "CFO",
+      "logo": "assets/img/teams/lcp/cfo.png"
+    },
+    "DCG": {
+      "name": "Relove Deep Cross Gaming",
+      "short": "DCG",
+      "logo": "assets/img/teams/lcp/dcg.png"
+    }
+  }
+};
